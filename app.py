5e8e91781eee982e6862269372ba8c5a434c539d
yogesh23 <yogesh10023@gmail.com>
2025-05-01 20:41:25 +0530

    s3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 import bcrypt
 from difflib import SequenceMatcher
 
-#hello.
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
